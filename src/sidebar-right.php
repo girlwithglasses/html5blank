a83f0b57343d86bab857b1eed7acaef55ad263a1
girlwithglasses <amelia.ireland@gmail.com>
2015-03-14 11:22:17 -0700

    Adding templates, removing sidebar-right

--- src/sidebar-right.php
+++
@@ -1,6 +0,0 @@
-<!-- sidebar right -->
-<aside class="sidebar" role="complementary">
-	<div class="sidebar-right">
-		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('right')) ?>
-	</div>
-</aside>
---
+++ src/templates/page_both-sidebar.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: Left and Right Sidebars
+*/
+	get_header();
+
+	get_sidebar('left'); ?>
+
+	<div id="primary-middle" role="main">
+		<!-- section -->
+		<section>
+			<!-- <h1><?php the_title(); ?></h1> -->
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<?php the_content(); ?>
+
+				<!-- ?php comments_template( '', true ); // Remove if you don't want comments ?>
+				<br class="clear">
+				<?php edit_post_link(); ?> -->
+
+			</article>
+		<?php endwhile; ?>
+		<?php else: ?>
+			<article>
+				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+			</article>
+		<?php endif; ?>
+		</section>
+	</div>
+
+<?php get_sidebar('right'); ?>
+
+<?php get_footer(); ?>
