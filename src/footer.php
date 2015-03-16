f8cc8a4cbe0e1c5e2d1b6aa11cbaf21859aa31ab
girlwithglasses <amelia.ireland@gmail.com>
2015-03-16 13:51:22 -0700

    Tidying up and abstracting typography options further

--- src/footer.php
+++ src/footer.php
@@ -1,4 +1,4 @@
-			<footer class="footer" role="contentinfo">
+			<footer class="footer C" role="contentinfo">
 				<p class="copyright">
 					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.
 				</p>
@@ -7,12 +7,21 @@
 		<!-- /wrapper -->
 		<?php wp_footer(); ?>
 		<script>
+
+	// conditionizr.com
+	// configure environment tests
+	conditionizr.config({
+		assets: '<?php echo get_template_directory_uri(); ?>',
+		tests: {}
+	});
+
 (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
 		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
 		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
 		ga('send', 'pageview');
+
 		</script>
 	</body>
 </html>
